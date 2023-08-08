5cbe4c292a1d9c5feec9993c646d35b56dca75f4
Hedi Kelesh <hedkel.git@gmail.com>
2023-08-08 14:36:50 +0200

    First Thread

--- src/FirstThread.java
+++ src/FirstThread.java
@@ -1,7 +1,23 @@
-public class FirstThread {
+public class FirstThread implements Runnable{
+
+    private final int date;
+    public FirstThread(int date){
+        this.date = date;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + " from thread: " + date);
 
 
-    public FirstThread(){}
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 
 
